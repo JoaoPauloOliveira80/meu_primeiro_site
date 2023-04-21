0f209810385d07e42f84e93d3d0d5ea3d4961f80
Dundaki <vigjoaopaulo@gmail.com>
2023-04-21 18:42:50 -0300

    inicio do projeto

---
+++ js/script.js
@@ -0,0 +1,19 @@
+console.log('funcionando')
+
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 100 * i)
+
+  });
+}
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
+
+
+
+    
